26dcd1a1120e4277bb07b9fced324da82448b0e2
Faruk Öztürk <infofaruk9@gmail.com>
2022-05-06 00:04:55 +0300

    Program temellerinin atılması

---
+++ app/main.py
@@ -0,0 +1,40 @@
+from discord.ext import commands
+from flask import Flask
+import multiprocessing as mlps
+
+
+
+app = Flask(__name__)
+flow = commands.Bot("!f ")
+
+@flow.event
+async def on_ready():
+    print("Flow started!")
+
+
+@flow.command()
+async def up(message):
+    await message.send("Api update edildi!")
+
+@app.route("/")
+def index():
+    return "Say Hello"
+
+
+token ="ODkxNjkxNzQ3OTI1Njg4NDMx.YVCCpw.iCBTSZzbuLkr4b4DJ36aVwnmG-U"
+def processOne():
+    flow.run(token)
+    
+def processTwo():
+    app.run(debug=False)
+
+p1 = mlps.Process(target=processOne)
+p2 = mlps.Process(target=processTwo)        
+
+if __name__ == "__main__":
+    p1.start()
+    p2.start()
+
+
+
+
